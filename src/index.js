95827672f55736fe5e8718fe49c2080a070e0622
VanePuentes <puentesvanessa00@gmail.com>
2024-05-17 20:36:54 -0500

    config Plantillas, rutas y ediciones

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 import express from 'express'
+import morgan from 'morgan'
+
+import {join,dirname} from 'path'
+import {fileURLToPath} from 'url'
+import {engine} from 'express-handlebars'
+
+
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 app.set('port', process.env.PORT || 3000 )
+app.set('views', join(__dirname, 'views'))
+app.engine('.hbs', engine({
+    defaultLayout: 'main',
+    layoutsDir: join(app.get ('views'), 'layauts'),
+    partialsDir: join(app.get ('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended:false}))
+app.use(express.json());
 
+app.get('/',(req,res) => {
+res.json({"message": "Hola Mundo"})
+});
 
-
-
-
-
-
-
-
+app.use(express.static(join(__dirname, 'public')))
 
 app.listen(app.get('port'),() => {
 console.log('server listening on port', app.get('port'));
