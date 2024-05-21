e72fd8bfb6addb4415c9bf40f216f97b090e6ad1
VanePuentes <puentesvanessa00@gmail.com>
2024-05-20 19:47:02 -0500

    Inicio pagina index y menu

--- src/index.js
+++ src/index.js
@@ -1,34 +1,41 @@
+/* -------------------------- Import -------------------------- */
 import express from 'express'
-import morgan from 'morgan'
 
-import {join,dirname} from 'path'
+import {join, dirname} from 'path'
 import {fileURLToPath} from 'url'
-import {engine} from 'express-handlebars'
+import {engine} from 'express-handlebars';
+import morgan from 'morgan';
 
-
+/* ------------------------------- initialization ------------------------------ */
 const app = express();
+/* ---------------------------- Evitar colisiones con los nombres de las variables --------------------------- */
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+/* --------------------------------- setting -------------------------------- */
 app.set('port', process.env.PORT || 3000 )
 app.set('views', join(__dirname, 'views'))
 app.engine('.hbs', engine({
     defaultLayout: 'main',
-    layoutsDir: join(app.get ('views'), 'layauts'),
-    partialsDir: join(app.get ('views'), 'partials'),
+    layoutsDir: join(app.get('views'), 'layouts') ,
+    partialsDir: join(app.get('views'), 'partials'),
     extname: '.hbs'
 }));
 app.set('view engine', '.hbs');
 
+/* ------------------------------- Middlewares ------------------------------ */
 app.use(morgan('dev'));
-app.use(express.urlencoded({ extended:false}))
-app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
-app.get('/',(req,res) => {
-res.json({"message": "Hola Mundo"})
+/* --------------------------------- routes --------------------------------- */
+app.get('/', (req, res)=> {
+    res.render('index')
 });
 
+/* ------------------------------ Public files ------------------------------ */
 app.use(express.static(join(__dirname, 'public')))
 
-app.listen(app.get('port'),() => {
-console.log('server listening on port', app.get('port'));
-} );
+/* ------------------------------- Run server ------------------------------- */
+app.listen(app.get('port'), () => {
+    console.log('Server listening on port', app.get('port'));
+});
