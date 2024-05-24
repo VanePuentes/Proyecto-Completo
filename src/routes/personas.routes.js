ecad96ab3273b04034bf311cf8eddd10d73a35fd
VanePuentes <puentesvanessa00@gmail.com>
2024-05-23 19:52:58 -0500

    se crea modulo de editar

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -7,18 +7,18 @@ router.get('/add',(req, res)=>{
 res.render('personas/add')
 });
 
-router.post('/add', async (req,res)=>{
- try {
-
-   const{name,lastname,age} = req.body  
-   const newPersona = {
-      name, lastname, age
-   } 
-} catch (error) {
-   res.status(500).json({message: error.message})
-}
+router.post('/add', async (req, res)=>{
+   try {
+       const { name, lastname, age} = req.body
+       const newPersona = {
+           name, lastname, age
+       }
+       await pool.query('INSERT INTO personas SET ?', [newPersona]);
+       res.redirect('/list');
+   } catch (error) {
+       res.status(500).json({ message: error.message });
+   }
 });
-
 
 router.get('/list',async(req,res) => {
 try {
@@ -32,11 +32,34 @@ try {
 router.get('/delete/:id', async (req, res) => {
    try {
        const { id } = req.params
-       await pool.query('INSERT INTO personas SET ?', [newPersona]);
+       await pool.query('DELETE FROM personas WHERE id = ?', [id]);
        res.redirect('/list');
    } catch (error) {
        res.status(500).json({ message: error.message });
    }
+});
+
+router.get('/edit/:id', async (req, res)=>{
+    try {
+        const {id} = req.params
+        const [persona] = await pool.query('SELECT * FROM personas WHERE id = ?', [id]);
+        const personaEdit = persona[0]
+        res.render('personas/edit', { persona: personaEdit })
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.post('/edit/:id', async (req, res)=>{
+    try {
+        const {id} = req.params
+        const {name, lastname, age}  = req.body
+        const editPersona = {name, lastname,age}
+        await pool.query('UPDATE personas SET ? WHERE id = ?', [editPersona, id]);
+        res.redirect('/list');
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
 
 export default router;
